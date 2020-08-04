ca1b68f444cca070bf8ca80702651ee9840f6388
Saloni Patil <salonivpatil2000@gmail.com>
2020-08-05 00:02:14 +0530

    second commit

---
+++ challenge2/main.js
@@ -0,0 +1,9 @@
+
+     const toggle = document.getElementById('toggle');
+     const menu = document.getElementById('menu');
+ 
+     toggle.addEventListener('click',() => {
+            toggle.classList.toggle('show');
+            menu.classList.toggle('show');
+     }
+     );
