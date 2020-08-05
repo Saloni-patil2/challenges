6c83461783910e7e2fd58a7951fc55f4374dc575
Saloni Patil <salonivpatil2000@gmail.com>
2020-08-06 00:53:39 +0530

    3 commit

--- challenge2/main.js
+++
@@ -1,9 +0,0 @@
-
-     const toggle = document.getElementById('toggle');
-     const menu = document.getElementById('menu');
- 
-     toggle.addEventListener('click',() => {
-            toggle.classList.toggle('show');
-            menu.classList.toggle('show');
-     }
-     );
